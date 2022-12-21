8ff98d27105e59ad29ef8d78b271e202ad6e3e39
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-20 22:09:40 -0500

    Created eventListener in javascript

---
+++ script.js
@@ -0,0 +1,13 @@
+//var showQuestions = document.getElementById()
+var startBttn = document.getElementById('#Start')
+document.getElementById('Start').addEventListener('click', startHere);
+
+
+function startHere() {
+    if (startBttn == startBttn){
+        console.log('You Guessed it!')
+    }
+        console.log("test")
+}
+
+
