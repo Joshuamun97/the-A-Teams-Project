9853852a23501352af6bffe44743a34c89cd0e81
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-21 22:37:19 -0500

    updated js to load questions on click

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 //var showQuestions = document.getElementById()
 var startBttn = document.getElementById('#Start')
-var startingHereTest = document.querySelector ('.startingHere')
+var gettingStarted = document.querySelector ('.startingHere')
+var bringOnTheQuestions = document.querySelector ('#hidden')
 document.getElementById('Start').addEventListener('click', startHere);
 
 
 function startHere() {
     if (startBttn == startBttn){
-        startingHereTest.classList.toggle('collapse');
+        gettingStarted.classList.toggle('collapse');
         console.log('You Guessed it!');
     }
         console.log("test")
+        openQuestions();
 }
 
 function openQuestions() {
-    if (startingHereTest == 'collapse') {
-        
-    }
+    bringOnTheQuestions.classList.toggle('collapse')
 }
+
 
 
