50a17246dba917d493c73d0db69c3e118843eb46
Joshua_Muniz <joshua.mun97@gmail.com>
2022-12-21 20:41:41 -0500

    added event listener to button

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 //var showQuestions = document.getElementById()
 var startBttn = document.getElementById('#Start')
+var startingHereTest = document.querySelector ('.startingHere')
 document.getElementById('Start').addEventListener('click', startHere);
 
 
 function startHere() {
     if (startBttn == startBttn){
-        console.log('You Guessed it!')
+        startingHereTest.classList.toggle('collapse');
+        console.log('You Guessed it!');
     }
         console.log("test")
+}
+
+function openQuestions() {
+    if (startingHereTest == 'collapse') {
+        
+    }
 }
 
 
