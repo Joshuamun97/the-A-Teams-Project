39959eeacf04ee8a836a78887c74c09aa5cb62ad
marksalazar <105082689+marksalazr@users.noreply.github.com>
2022-12-23 13:19:32 -0700

    clearing out my files so i can merge

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 
-//var showQuestions = document.getElementById()
-var startBttn = document.getElementById('#Start')
-var gettingStarted = document.querySelector ('.startingHere')
-var bringOnTheQuestions = document.querySelector ('#hidden')
-document.getElementById('Start').addEventListener('click', startHere);
-
-
-function startHere() {
-    if (startBttn == startBttn){
-        gettingStarted.classList.toggle('collapse');
-        console.log('You Guessed it!');
-    }
-        console.log("test")
-        openQuestions();
-}
-
-function openQuestions() {
-    bringOnTheQuestions.classList.toggle('collapse')
-}
-
-
-
 
