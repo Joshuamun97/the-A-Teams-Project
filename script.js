2dfc6c50e8ffa9951a5005cb6325966d52c4e2a0
marksalazar <105082689+marksalazr@users.noreply.github.com>
2022-12-23 11:29:51 -0700

    resolving my git

--- script.js
+++ script.js
@@ -1,70 +1,4 @@
-<<<<<<< HEAD
-// var showQuestions = document.getElementById()
 
-// var startBttn = document.getElementbyID('#Start')
-// document.getElementbyID('#Start').addEventListener('click', startHere);
-
-// function startHere() {
-//     if(startBttn==startBttn){
-//         startBttn.classList.add('hiddenOne');
-//     }
-// }
-
-// -------MARKS INPUT BELOW -------------------------------------------------
-
-
-
-var YesBttn = document.querySelector("#YesBttn");
-
-var NoBttn = document.querySelector("#NoBttn");
-
-
-
-
-// var SecondYesBttn = document.querySelector("#2ndYesBttn"); 
-// var SecondNoBttn = document.querySelector("#2ndNoBttn");
-
-// ^^^^these will show an error in console is because its collapesd/hidden  ?
-
-
-
-YesBttn.addEventListener("click", function (){
-    console.log ("you pressed the yes button!")
-});
-
-NoBttn.addEventListener( "click", function (){
-    console.log("you pressed the no button !")
-});
-
-function InARush() {
-    // if (YesBttn==YesBttn)
-    // run function with api to get nearby fastfood 
-
-    //else () //take user to followup questions )
-};
-
-SecondYesBttn.addEventListener("click", function (){
-    console.log("you pressed the yes button again !!")
-});
-
-SecondNoBttn.addEventListener("click", function (){
-    console.log("you pressed the no button again !!")
-})
-
-function Fancy (){};
-
-
-
-
-// if user selects yes to fancy <----- POSSIBLE IF STATEMENT 
-// run fancy function 
-
-// else run function for ... 
-
-//  NOTE ! WE SHOULD SEE HOW WE CAN USE GEO-LOCATION TO BE ABLE TO FIND NEARBY RESTAURANTS. 
-// OR WE ASK USER TO PUT IN THERE LOCATION  
-
-=======
 //var showQuestions = document.getElementById()
 var startBttn = document.getElementById('#Start')
 var gettingStarted = document.querySelector ('.startingHere')
@@ -87,4 +21,4 @@ function openQuestions() {
 
 
 
->>>>>>> main
+
