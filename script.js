ef8874d9e0cc5dae9d77ebfd5db4299610820a76
marksalazar <105082689+marksalazr@users.noreply.github.com>
2022-12-31 13:28:17 -0800

    wraped api's into two functions

--- script.js
+++ script.js
@@ -49,9 +49,9 @@ document.querySelector('#btn1').addEventListener('click', setNextQuestion);
 document.querySelector('#btn2').addEventListener('click', setNextQuestion);
 
 function getUserCriteria() {
-    var ans = ;
-    var userChoseAllergies = ;
-    var userChoseGenre = ;
+    var ans 
+    var userChoseAllergies 
+    var userChoseGenre  
 
 }
 
@@ -63,11 +63,46 @@ function gatherQuestions() {
     
 }
 // movie/show api 
-const MovieOptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'da507b3d2amsh4153460d232f196p18925bjsn195d14c68c13',
-		'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
-	}
+
+function findSomethingToWatch(){
+    var Options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'da507b3d2amsh4153460d232f196p18925bjsn195d14c68c13',
+            'X-RapidAPI-Host': 'utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com'
+        }
+    };
+var requestUrl = 'https://utelly-tv-shows-and-movies-availability-v1.p.rapidapi.com/lookup?term=Tropic%20Thunder&country=us'
+fetch(requestUrl, Options)
+.then(response => response.json())
+.then(response => console.log(response))
+.catch(err => console.error(err))
+for(var i = 0; i < data.results.length; i++){
+    var movieOrShow = document.createElement('p');
+    movieOrShow.textContent = data.results.term[i] // <<< I used .term because this its the parameter used in the api url 
+    append(movieOrShow);
+} 
 };
+//^^ function for movie api^^//
+
+
+function getRecipes(){
+const Options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'da507b3d2amsh4153460d232f196p18925bjsn195d14c68c13',
+        'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+    }
+ };
+var requestUrl = 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query=tacos&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&ignorePantry=true&sort=calories&sortDirection=asc&ranking=2'
+fetch( requestUrl, Options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+    var ChosenRecipe = document.createElement('p');
+    ChosenRecipe.textContent = data.results.query[i] // I used .query because its the parameter used in the url 
+    append(ChosenRecipe);
+};
+    
+    
 
